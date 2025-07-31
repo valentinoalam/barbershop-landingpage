c70b3e62c90f345acb9a3fec4106ed4b9c85d15c
Ichikyube <ichikyube@gmail.com>
2025-07-31 10:37:01 +0700

    turn section into jsx

--- src/components/cards/gallery-img.tsx
+++ src/components/cards/gallery-img.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function GalleryImage({ imageUrl }) {
+function GalleryImage({ imageUrl }: { imageUrl: string }) {
   return (
     <div className="flex flex-col gap-3">
       <div
