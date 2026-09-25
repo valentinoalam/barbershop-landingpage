34979e2f1dd3e9fa9c5275705e845bb25a7c7300
Ichikyube <ichikyube@gmail.com>
2026-09-25 15:12:40 +0700

    ta

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,19 @@
-import { PrismaClient } from "@prisma/client"
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// Dapatkan URL koneksi & berikan fallback aman agar build Vercel tidak crash
+const connectionString = process.env.DATABASE_URL || "postgresql://placeholder:placeholder@localhost:5432/db";
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+// ⚠️ PASSING 'adapter' KE PrismaClient MANDATORI DI PRISMA 7
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
